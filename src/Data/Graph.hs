6cab710fe4788482ecd65453b011f414d91186d3
Alexandra Ryzhevich <larandaansil@gmail.com>
2020-02-19 22:53:07 +0300

    Make building a graph safe

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Data.Graph where
 
@@ -6,16 +7,20 @@ import Control.Monad
 import Control.Monad.ST
 import qualified Control.Monad.State as State
 import Control.Monad.State (State)
-import qualified Data.Vector.Mutable as VM
 import qualified Data.Vector as V
+import qualified Data.Vector.Generic as VG
+import qualified Data.Vector.Mutable as VM
 import Data.Vector ((!))
 import Data.STRef
+import qualified Data.Queue.Mutable as QM
 
-type Vertex = Int
+newtype Vertex s = Vertex { unvertex :: Int }
+
+type Node = Int
 
 data Edge b = Edge
-    { eTo :: Vertex
-    , eFrom :: Vertex
+    { eTo :: Node
+    , eFrom :: Node
     , eVal :: b
     }
 
@@ -26,7 +31,7 @@ eAdj_ e = Adj
     }
 
 data Adj b = Adj
-    { aTo :: Vertex
+    { aTo :: Node
     , aVal :: b
     }
 
@@ -35,13 +40,12 @@ data Graph a b = Graph
     , adjs :: V.Vector (V.Vector (Adj b))
     }
 
-
--- build :: (Vertec -> a) -> [Edge b]
+-- build :: (Vertex -> a) -> [Edge b]
 -- build :: [(a, [(b, Offset)])]
 -- build :: [a] -> [Edge b]
 
-buildFromList :: [a] -> [Edge b] -> Graph a b
-buildFromList vs es = Graph
+buildFromList_ :: [a] -> [Edge b] -> Graph a b
+buildFromList_ vs es = Graph
     { verts = vs' 
     , adjs = V.map V.fromList es'
     }
@@ -54,18 +58,18 @@ buildFromList vs es = Graph
             VM.modify esL ((eAdj_ e):) (eFrom e)
         return esL
 
-data GraphBuilderState a b = GraphBuilderState
+data GraphBuilderState s a b = GraphBuilderState
     { gbsCount :: Int
     , gbsVerts :: [a]
     , gbsEdges :: [Edge b]
     }
 
-newtype GraphBuilder a b t = GraphBuilder (State (GraphBuilderState a b) t)
+newtype GraphBuilder s a b t = GraphBuilder (State (GraphBuilderState s a b) t)
     deriving (Functor, Applicative, Monad)
 
-build :: GraphBuilder a b t -> Graph a b
+build :: (forall s. GraphBuilder s a b t) -> Graph a b
 build (GraphBuilder builder) =
-    buildFromList (gbsVerts state) (gbsEdges state)
+    buildFromList_ (reverse . gbsVerts $ state) (gbsEdges state)
   where
     state = State.execState builder $ GraphBuilderState
         { gbsCount = 0
@@ -73,21 +77,19 @@ build (GraphBuilder builder) =
         , gbsEdges = []
         }
 
-vertex :: a -> GraphBuilder a b Vertex
+vertex :: a -> GraphBuilder s a b (Vertex s)
 vertex v = GraphBuilder $ do
     state <- State.get
     State.put $ state
         { gbsCount = gbsCount state + 1
         , gbsVerts = v:(gbsVerts state)
         }
-    pure (gbsCount state)
+    pure (Vertex (gbsCount state))
 
-edge :: Vertex -> Vertex -> b -> GraphBuilder a b ()
-edge v u l = GraphBuilder $ do
+edge :: Vertex s -> Vertex s -> b -> GraphBuilder s a b ()
+edge (Vertex v) (Vertex u) l = GraphBuilder $ do
     state <- State.get
-    if (v >= gbsCount state || u >= gbsCount state) 
-    then error "Incorrect edge"
-    else State.put $ state
+    State.put $ state
         { gbsEdges = edge:(gbsEdges state)
         }
   where
@@ -97,12 +99,46 @@ edge v u l = GraphBuilder $ do
         , eVal = l
         }
 
+numVertices :: Graph a b -> Int
+numVertices = V.length . verts
 
-numVerteces :: Graph a b -> Int
-numVerteces = V.length . adjs
+vertices :: Graph a b -> [a]
+vertices = V.toList . verts
 
-numEdges :: Graph a b -> Int
-numEdges = V.sum . V.map V.length . adjs
+edges :: Graph a b -> [(a, b, a)]
+edges g = V.toList . V.concat . V.toList . V.imap toTuples . adjs $ g
+  where
+    toTuples i = V.map (\adj -> ((verts g) ! i, aVal adj, (verts g) ! (aTo adj)))
 
-deg :: Int -> Graph a b -> Int
-deg v = V.length . (! v) . adjs
+degree :: Graph a b -> Graph (a, Int) b
+degree g = Graph
+    { verts = V.imap (\i val -> (val, V.length ((adjs g) ! i))) (verts g)
+    , adjs = adjs g
+    }
+
+bfs_ :: [Node] -> Graph a b -> (V.Vector Node, V.Vector Int)
+bfs_ vs g = runST $ do
+    q <- QM.new
+    ordToV <- QM.new
+    vToOrd <- VM.replicate n n
+    curOrd <- newSTRef 0
+    mapM_ (QM.push q) vs
+    loop q ordToV vToOrd curOrd
+    ordToVList <- QM.drain ordToV
+    vToOrdList <- mapM (VM.read vToOrd) [0..(n - 1)]
+    return (V.fromList ordToVList, V.fromList vToOrdList)
+  where
+    n = numVertices g
+    loop q ordToV vToOrd curOrd = do
+        qEmpty <- QM.empty q
+        if qEmpty
+            then return ()
+            else do
+                v <- QM.pop q
+                QM.push ordToV v
+                ord <- readSTRef curOrd
+                VM.write vToOrd v ord
+                modifySTRef curOrd (+ 1)
+                V.forM_ ((adjs g) ! v) $ \adjU -> do
+                    uOrd <- VM.read vToOrd (aTo adjU)
+                    when (uOrd == n) $ QM.push q (aTo adjU)
--- test/Data/GraphSpec.hs
+++ test/Data/GraphSpec.hs
@@ -5,33 +5,77 @@ import qualified Data.Graph as G
 import Test.Hspec
 
 
-buildFromListSpec :: Spec
-buildFromListSpec = describe "buildFromList" $ do
+verticesSpec :: Spec
+verticesSpec = describe "vertices" $ do
 
-    it "should create empty graph" $ do
+    it "should be empty for empty graph" $ do
 
-        let g = G.buildFromList [] []
+        let g = G.build (pure ())
 
-        (G.numVerteces g) `shouldBe` 0
-        (G.numEdges g) `shouldBe` 0
+        G.vertices g `shouldBe` ([] :: [()])
 
-    it "should create graph with all verteces isolated" $ do
+    it "should return all vertices for graph without edges" $ do
 
-        let g = G.buildFromList [(), ()] []
+        let g = G.build $ do {
+            G.vertex "a";
+            G.vertex "b";
+            pure ()
+        }
 
-        (G.numVerteces g) `shouldBe` 2
-        (G.numEdges g) `shouldBe` 0
+        G.vertices g `shouldMatchList` ["a", "b"]
 
-    it "should create graph with different vertex degrees" $ do
+    it "should return vertices with repeated labels" $ do
 
-        let g = G.buildFromList [(), (), ()] [G.Edge 0 1 (), G.Edge 0 2 (), G.Edge 1 2 ()]
+        let g = G.build $ do {
+            G.vertex "a";
+            G.vertex "b";
+            G.vertex "a";
+            pure ()
+        }
 
-        (G.numVerteces g) `shouldBe` 3
-        (G.numEdges g) `shouldBe` 3
-        (G.deg 0 g) `shouldBe` 0
-        (G.deg 1 g) `shouldBe` 1
-        (G.deg 2 g) `shouldBe` 2
+        G.vertices g `shouldMatchList` ["a", "a", "b"]
 
+    it "should return all vertices for graph with edges" $ do
+
+        let g = G.build $ do {
+            a <- G.vertex "a";
+            b <- G.vertex "b";
+            G.edge a b ()
+        }
+
+        G.vertices g `shouldMatchList` ["a", "b"]
+
+edgesSpec :: Spec
+edgesSpec = describe "edges" $ do
+
+    it "should be empty for empty graph" $ do
+
+        let g = G.build (pure ())
+
+        G.edges g `shouldBe` ([] :: [((), (), ())])
+
+    it "should be empty for graph without edges" $ do
+
+        let g = G.build $ do {
+            G.vertex "a";
+            G.vertex "b";
+            pure ()
+        }
+
+        G.edges g `shouldBe` ([] :: [(String, (), String)])
+
+    it "should return all edges for graph with edges" $ do
+
+        let g = G.build $ do {
+            a <- G.vertex "a";
+            b <- G.vertex "b";
+            c <- G.vertex "c";
+            G.edge a b ();
+            G.edge a c ();
+            G.edge b c ()
+        }
+
+        G.edges g `shouldMatchList` [("a", (), "b"), ("a", (), "c"), ("b", (), "c")]
 
 buildSpec :: Spec
 buildSpec = describe "build" $ do
@@ -40,45 +84,39 @@ buildSpec = describe "build" $ do
 
         let g = G.build (pure ())
 
-        (G.numVerteces g) `shouldBe` 0
-        (G.numEdges g) `shouldBe` 0
+        length (G.vertices g) `shouldBe` 0
+        length (G.edges g) `shouldBe` 0
 
     it "should create graph with all verteces isolated" $ do
 
         let g = G.build $ do {
             G.vertex ();
-            G.vertex ()
+            G.vertex ();
+            pure ()
         }
 
-        (G.numVerteces g) `shouldBe` 2
-        (G.numEdges g) `shouldBe` 0
+        length (G.vertices g) `shouldBe` 2
+        length (G.edges g) `shouldBe` 0
 
     it "should create graph with different vertex degrees" $ do
 
         let g = G.build $ do {
-            v0 <- G.vertex ();
-            v1 <- G.vertex ();
-            v2 <- G.vertex ();
+            a <- G.vertex "a";
+            b <- G.vertex "b";
+            c <- G.vertex "c";
 
-            G.edge v1 v0 ();
-            G.edge v2 v0 ();
-            G.edge v2 v1 ()
+            G.edge b a ();
+            G.edge c a ();
+            G.edge c b ()
         }
 
-        (G.numVerteces g) `shouldBe` 3
-        (G.numEdges g) `shouldBe` 3
-        (G.deg 0 g) `shouldBe` 0
-        (G.deg 1 g) `shouldBe` 1
-        (G.deg 2 g) `shouldBe` 2
+        length (G.vertices g) `shouldBe` 3
+        length (G.edges g) `shouldBe` 3
+        G.vertices (G.degree g) `shouldMatchList` [("a", 0), ("b", 1), ("c", 2)]
 
-    it "should cause error because of incorrect edge usage" $ do
-        action `shouldThrow` errorCall "Incorrect edge"
-      where
-        action = do
-            let g = G.build $ (G.edge 0 1 ())
-            (G.numVerteces g) `shouldBe` 0
 
 spec :: Spec
 spec = describe "Data.Graph" $ do
-    buildFromListSpec
     buildSpec
+    verticesSpec
+    edgesSpec
