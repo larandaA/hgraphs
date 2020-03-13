dbb3616635c2c5fe017cb08294917f09535a2211
Alexandra Ryzhevich <larandaansil@gmail.com>
2020-03-14 01:38:39 +0300

    Move common graph definitions to a separate module

--- src/Data/Graph/Abstract.hs
+++ src/Data/Graph/Abstract.hs
@@ -223,39 +223,3 @@ transformd isStart f defaultVal g = g { verts = vs }
             VM.write vsV v (f bParents (verts g ! v))
         flip V.imapM_ vToOrd $ \v ordV -> when (ordV == n) (VM.write vsV v (defaultVal (verts g ! v)))
         return vsV
-
-
-isolated :: [v] -> Graph e v
-isolated ls = build $ mapM_ vertex ls
-
-singleton :: v -> Graph e v
-singleton l = isolated [l]
-
-path :: [v] -> Graph' v
-path [] = isolated []
-path ls = build $ do
-    vs <- mapM vertex ls
-    mapM (uncurry edge') (L.zip vs (L.tail vs))
-
-cycle :: [v] -> Graph' v
-cycle [] = isolated []
-cycle [l] = singleton l
-cycle (l:ls) = build $ do
-    v <- vertex l
-    vs <- mapM vertex ls
-    mapM (uncurry edge') (L.zip ([v] ++ vs) (vs ++ [v]))
-
-clique :: [v] -> Graph' v
-clique ls = build $ do
-    vs <- mapM vertex ls
-    let ivs = L.zip [1..] vs
-    sequence_ [edge' av bv | (i, av) <- ivs, (j, bv) <- ivs, i /= j]
-
-biclique :: [v] -> [v] -> Graph' v
-biclique als bls = build $ do
-    avs <- mapM vertex als
-    bvs <- mapM vertex bls
-    sequence_ [edge' av bv | av <- avs, bv <- bvs]
-
-star :: v -> [v] -> Graph' v
-star l = biclique [l]
---
+++ src/Data/Graph/Abstract/Common.hs
@@ -0,0 +1,39 @@
+module Data.Graph.Abstract.Common where
+
+import Data.Graph.Abstract
+import qualified Data.List as L
+
+isolated :: [v] -> Graph e v
+isolated ls = build $ mapM_ vertex ls
+
+singleton :: v -> Graph e v
+singleton l = isolated [l]
+
+path :: [v] -> Graph' v
+path [] = isolated []
+path ls = build $ do
+    vs <- mapM vertex ls
+    mapM (uncurry edge') (L.zip vs (L.tail vs))
+
+cycle :: [v] -> Graph' v
+cycle [] = isolated []
+cycle [l] = singleton l
+cycle (l:ls) = build $ do
+    v <- vertex l
+    vs <- mapM vertex ls
+    mapM (uncurry edge') (L.zip ([v] ++ vs) (vs ++ [v]))
+
+clique :: [v] -> Graph' v
+clique ls = build $ do
+    vs <- mapM vertex ls
+    let ivs = L.zip [1..] vs
+    sequence_ [edge' av bv | (i, av) <- ivs, (j, bv) <- ivs, i /= j]
+
+biclique :: [v] -> [v] -> Graph' v
+biclique als bls = build $ do
+    avs <- mapM vertex als
+    bvs <- mapM vertex bls
+    sequence_ [edge' av bv | av <- avs, bv <- bvs]
+
+star :: v -> [v] -> Graph' v
+star l = biclique [l]
