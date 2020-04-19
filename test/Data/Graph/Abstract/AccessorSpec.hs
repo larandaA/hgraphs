8b048dfd8e7aa6909c42af27183f965d3fcac793
Alexandra Ryzhevich <larandaansil@gmail.com>
2020-04-19 23:29:41 +0300

    Add tests for vertex accessors

--- test/Data/Graph/Abstract/AccessorSpec.hs
+++ test/Data/Graph/Abstract/AccessorSpec.hs
@@ -2,6 +2,7 @@ module Data.Graph.Abstract.AccessorSpec (spec) where
 
 import qualified Data.Graph.Abstract as GA
 import qualified Data.Graph.Abstract.Accessor as GAA
+import qualified Data.Graph.Abstract.Builder as GAB
 import qualified Data.Graph.Abstract.Common as GAC
 import Data.STRef
 import Test.Hspec
@@ -38,8 +39,128 @@ executeSpec = describe "execute" $ do
 
         x `shouldBe` "x"
 
+verticesSpec :: Spec
+verticesSpec = describe "vertices" $ do
+
+    it "should return an empty list for an empty graph" $ do
+
+        let g = GAC.empty
+        let len = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            pure (length vs)
+        }
+
+        len `shouldBe` 0
+
+    it "should return all the vertices" $ do
+
+        let g = GAC.isolated [1, 3, 5, 9]
+        let values = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            traverse GAA.value vs
+        }
+
+        values `shouldMatchList` [1, 3, 5, 9]
+
+valueSpec :: Spec
+valueSpec = describe "value" $ do
+
+    it "should return a value of the only vertex in a trivial graph" $ do
+
+        let g = GAC.singleton "x"
+        let x = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            GAA.value (head vs)
+        }
+
+        x `shouldBe` "x"
+
+    it "should return different values for different vertices" $ do
+
+        let g = GAC.isolated [1, 3, 5, 9]
+        let values = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            traverse GAA.value vs
+        }
+
+        values `shouldMatchList` [1, 3, 5, 9]
+
+successorsSpec :: Spec
+successorsSpec = describe "successors" $ do
+
+    it "should return an empty list for the only vertex in a trivial graph" $ do
+
+        let g = GAC.singleton ()
+        let numSuccs = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            succs <- GAA.successors (head vs);
+            pure (length succs)
+        }
+
+        numSuccs `shouldBe` 0
+
+    it "should return correct successors" $ do
+
+        let g = GAB.build $ do {
+            v0 <- GAB.vertex 0;
+            v1 <- GAB.vertex 1;
+            v2 <- GAB.vertex 2;
+
+            GAB.edge' v0 v1;
+            GAB.edge' v0 v2;
+            GAB.edge' v1 v2
+        }
+
+        let edges = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            vals <- traverse GAA.value vs;
+            succs <- traverse GAA.successors vs;
+            succVals <- traverse (traverse GAA.value) succs;
+            pure (concat (zipWith (\v s -> map (\sv -> (v, sv)) s) vals succVals))
+        }
+
+        edges `shouldMatchList` [(0, 1), (1, 2), (0, 2)]
+
+degreeSpec :: Spec
+degreeSpec = describe "degree" $ do
+
+    it "should be zero for the only vertex in a trivial graph" $ do
+
+        let g = GAC.singleton ()
+        let degree = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            GAA.degree (head vs)
+        }
+
+        degree `shouldBe` 0
+
+    it "should return different degrees for all vertices" $ do
+
+        let g = GAB.build $ do {
+            a <- GAB.vertex "a";
+            b <- GAB.vertex "b";
+            c <- GAB.vertex "c";
+
+            GAB.edge' a b;
+            GAB.edge' a c;
+            GAB.edge' b c
+        }
+
+        let degrees = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            vals <- traverse GAA.value vs;
+            degs <- traverse GAA.degree vs;
+            pure (zip vals degs)
+        }
+
+        degrees `shouldMatchList` [("a", 2), ("b", 1), ("c", 0)]
+
 
 spec :: Spec
 spec = describe "Data.Graph.Abstract.Accessor" $ do
     liftSTSpec
     executeSpec
+    verticesSpec
+    valueSpec
+    successorsSpec
+    degreeSpec
