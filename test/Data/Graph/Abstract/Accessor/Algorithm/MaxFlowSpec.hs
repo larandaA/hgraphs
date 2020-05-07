6539391494e2596debc0a0b37184fc0d9771fee2
Alexandra Ryzhevich <larandaansil@gmail.com>
2020-05-07 23:07:12 +0300

    Use the same test cases for all max flow algorithms

--- test/Data/Graph/Abstract/Accessor/Algorithm/MaxFlowSpec.hs
+++ test/Data/Graph/Abstract/Accessor/Algorithm/MaxFlowSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Data.Graph.Abstract.Accessor.Algorithm.MaxFlowSpec (spec) where
 
 import Control.Monad
@@ -8,9 +10,28 @@ import qualified Data.Graph.Abstract.Builder as GAB
 import qualified Data.Graph.Abstract.Common as GAC
 import Test.Hspec
 
+type MaxFlow s e v = GAA.Vertex s
+                  -> GAA.Vertex s
+                  -> (GAA.Edge s -> GAA.Accessor s e v Int)
+                  -> GAA.Accessor s e v (GAA.EArray s Int)
 
-edkarpSpec :: Spec
-edkarpSpec = describe "edkarp" $ do
+maxflowSpec :: (forall s e v. MaxFlow s e v) -> String -> Spec
+maxflowSpec algorithm name = describe name $ do
+
+    it "should work fine on source and sink being the same vertex" $ do
+
+        let g = GAC.path [1, 2, 3, 4]
+
+        let flow = GAA.execute g $ do {
+            source <- GAA.vfind (== 2);
+            sink <- GAA.vfind (== 2);
+            flow <- algorithm (head source) (head sink) (const (pure 1));
+            edges <- GAA.outgoing (head source);
+            flow' <- traverse (GAA.eget flow) edges;
+            pure (foldr (+) 0 flow')
+        }
+
+        flow `shouldBe` 0
 
     it "should work fine on graph without edges" $ do
 
@@ -19,7 +40,7 @@ edkarpSpec = describe "edkarp" $ do
         let flow = GAA.execute g $ do {
             source <- GAA.vfind (== 1);
             sink <- GAA.vfind (== 4);
-            flow <- MaxFlow.edkarp (head source) (head sink) (const (pure 1));
+            flow <- algorithm (head source) (head sink) (const (pure 1));
             edges <- GAA.outgoing (head source);
             flow' <- traverse (GAA.eget flow) edges;
             pure (foldr (+) 0 flow')
@@ -34,7 +55,7 @@ edkarpSpec = describe "edkarp" $ do
         let (g', flow) = GAA.execute g $ do {
             source <- GAA.vfind (== 1);
             sink <- GAA.vfind (== 4);
-            flow <- MaxFlow.edkarp (head source) (head sink) (const (pure 0));
+            flow <- algorithm (head source) (head sink) (const (pure 0));
             edges <- GAA.outgoing (head source);
             flow' <- traverse (GAA.eget flow) edges;
             g' <- GAA.egraph flow;
@@ -60,7 +81,7 @@ edkarpSpec = describe "edkarp" $ do
         let (g', flow) = GAA.execute g $ do {
             source <- GAA.vfind (== 0);
             sink <- GAA.vfind (== 3);
-            flow <- MaxFlow.edkarp (head source) (head sink) (GAA.label);
+            flow <- algorithm (head source) (head sink) (GAA.label);
             edges <- GAA.outgoing (head source);
             flow' <- traverse (GAA.eget flow) edges;
             g' <- GAA.egraph flow;
@@ -86,7 +107,7 @@ edkarpSpec = describe "edkarp" $ do
         let (g', flow) = GAA.execute g $ do {
             source <- GAA.vfind (== 0);
             sink <- GAA.vfind (== 3);
-            flow <- MaxFlow.edkarp (head source) (head sink) (GAA.label);
+            flow <- algorithm (head source) (head sink) (GAA.label);
             edges <- GAA.outgoing (head source);
             flow' <- traverse (GAA.eget flow) edges;
             g' <- GAA.egraph flow;
@@ -115,7 +136,7 @@ edkarpSpec = describe "edkarp" $ do
         let flow = GAA.execute g $ do {
             source <- GAA.vfind (== 0);
             sink <- GAA.vfind (== 4);
-            flow <- MaxFlow.edkarp (head source) (head sink) (GAA.label);
+            flow <- algorithm (head source) (head sink) (GAA.label);
             edges <- GAA.outgoing (head source);
             flow' <- traverse (GAA.eget flow) edges;
             pure (foldr (+) 0 flow')
@@ -123,7 +144,7 @@ edkarpSpec = describe "edkarp" $ do
 
         flow `shouldBe` 4
 
-    it "should correct flow in graph with cycles" $ do
+    it "should find correct flow in graph with cycles" $ do
 
         let g = GAB.build $ do {
             a <- GAB.vertex "a";
@@ -152,7 +173,7 @@ edkarpSpec = describe "edkarp" $ do
         let (g', flow) = GAA.execute g $ do {
             source <- GAA.vfind (== "a");
             sink <- GAA.vfind (== "g");
-            flow <- MaxFlow.edkarp (head source) (head sink) (GAA.label);
+            flow <- algorithm (head source) (head sink) (GAA.label);
             edges <- GAA.outgoing (head source);
             flow' <- traverse (GAA.eget flow) edges;
             g' <- GAA.egraph flow;
@@ -170,7 +191,39 @@ edkarpSpec = describe "edkarp" $ do
             , ("h", 4, "f")
             ]
 
+    it "should find correct flow in graph multiple shortest paths" $ do
+
+        let g = GAB.build $ do {
+            s <- GAB.vertex "s";
+            v1 <- GAB.vertex "1";
+            v2 <- GAB.vertex "2";
+            v3 <- GAB.vertex "3";
+            v4 <- GAB.vertex "4";
+            t <- GAB.vertex "t";
+
+            GAB.edge 10 s v1;
+            GAB.edge 10 s v2;
+            GAB.edge 2 v1 v2;
+            GAB.edge 4 v1 v3;
+            GAB.edge 8 v1 v4;
+            GAB.edge 9 v2 v4;
+            GAB.edge 10 v3 t;
+            GAB.edge 6 v4 v3;
+            GAB.edge 10 v4 t
+        }
+
+        let flow = GAA.execute g $ do {
+            source <- GAA.vfind (== "s");
+            sink <- GAA.vfind (== "t");
+            flow <- algorithm (head source) (head sink) (GAA.label);
+            edges <- GAA.outgoing (head source);
+            flow' <- traverse (GAA.eget flow) edges;
+            pure (foldr (+) 0 flow')
+        }
+
+        flow `shouldBe` 19
+
 
 spec :: Spec
 spec = describe "Data.Graph.Abstract.Accessor.Algorithm.MaxFlow" $ do
-    edkarpSpec
+    maxflowSpec MaxFlow.edkarp "edkarp"
