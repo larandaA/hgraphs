ba3a00fce2d9e955c64ed2025ce246f13bd9ea1c
Alexandra Ryzhevich <larandaansil@gmail.com>
2020-05-11 17:30:29 +0300

    Implement Bellman-Ford for accessor monad

--- src/Data/Graph/Abstract/Accessor/Algorithm/Distance.hs
+++ src/Data/Graph/Abstract/Accessor/Algorithm/Distance.hs
@@ -1,5 +1,5 @@
 module Data.Graph.Abstract.Accessor.Algorithm.Distance
-    ( dijkstra
+    ( dijkstra, bford
     ) where
 
 import Control.Monad
@@ -8,6 +8,7 @@ import Control.Monad.ST (ST)
 import Data.Graph.Abstract.Accessor
 import qualified Data.Graph.Abstract.Accessor.PQueue as PQueue
 import qualified Data.Graph.Abstract.Accessor.Queue as Queue
+import qualified Data.Graph.Abstract.Accessor.Ref as Ref
 import Data.Maybe (isJust)
 import Data.Ord (comparing)
 
@@ -32,3 +33,41 @@ dijkstra starts cost = do
                 PQueue.push queue (successor, distance')
 
     pure distances
+
+bford :: (Num a, Ord a) => [Vertex s] -> (e -> a)
+      -> Accessor s e v (Maybe (VArray s (Maybe a)))
+bford starts cost = do
+    distances <- varray Nothing
+    costs <- ebuild (fmap cost . label)
+    let relax = relax' distances costs
+    edges <- edges
+    n <- length <$> vertices
+
+    forM_ starts $ \start -> vset distances start (Just 0)
+
+    forM_ [0..n - 1] $ \_ -> do
+        forM_ edges $ \edge -> do
+            relaxed <- relax edge
+            when (isJust relaxed) $ do
+                target' <- target edge
+                vset distances target' relaxed
+
+    negative <- Ref.new False
+    forM_ edges $ \edge -> do
+        negative' <- isJust <$> relax edge
+        when negative' $ Ref.set negative True
+
+    negative <- Ref.get negative
+    if negative then (pure Nothing) else (pure (Just distances))
+  where
+    relax' distances costs edge = do
+        cost <- eget costs edge
+        sdistance <- vget distances (source edge)
+        target' <- target edge
+        tdistnace <- vget distances target'
+        case (sdistance, tdistnace) of
+            (Nothing, _) -> pure Nothing
+            (Just sd, Nothing) -> pure (Just (sd + cost))
+            (Just sd, Just td) -> if (sd + cost < td)
+                                      then pure (Just (sd + cost))
+                                      else pure Nothing
--- test/Data/Graph/Abstract/Accessor/Algorithm/DistanceSpec.hs
+++ test/Data/Graph/Abstract/Accessor/Algorithm/DistanceSpec.hs
@@ -5,6 +5,7 @@ import qualified Data.Graph.Abstract.Accessor as GAA
 import qualified Data.Graph.Abstract.Accessor.Algorithm.Distance as Dist
 import qualified Data.Graph.Abstract.Builder as GAB
 import qualified Data.Graph.Abstract.Common as GAC
+import Data.Maybe (isJust, isNothing, fromJust)
 import Test.Hspec
 
 
@@ -149,7 +150,215 @@ dijkstraSpec = describe "dijkstra" $ do
             , (4, Just 5), (5, Just 10)
             ]
 
+bfordSpec :: Spec
+bfordSpec = describe "bford" $ do
+
+    it "should be Nothing for all vertices on empty list of start vertices" $ do
+
+        let g = GAC.isolated [1, 2, 3, 4]
+
+        let mg' = GAA.execute g $ do {
+            distances <- Dist.bford [] (const 1);
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices g' `shouldMatchList` [Nothing, Nothing, Nothing, Nothing]
+
+    it "should be 0 for all vertices if all vertices are start vertices" $ do
+
+        let g = GAC.isolated [1, 2, 3, 4]
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vertices;
+            distances <- Dist.bford vs (const 1);
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices g' `shouldMatchList` [Just 0, Just 0, Just 0, Just 0]
+
+    it "should be 0 for a single vertex on isolated vertices" $ do
+
+        let g = GAC.isolated [1, 2, 3, 4]
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 3);
+            distances <- Dist.bford vs (const 1);
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices (GA.zip g g') `shouldMatchList`
+            [ (1, Nothing), (2, Nothing)
+            , (3, Just 0), (4, Nothing)
+            ]
+
+    it "should be 1 for rays of a star" $ do
+
+        let g = GAC.star 4 [1, 2, 3]
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 4);
+            distances <- Dist.bford vs (const 1);
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices (GA.zip g g') `shouldMatchList`
+            [ (1, Just 1), (2, Just 1)
+            , (3, Just 1), (4, Just 0)
+            ]
+
+    it "should be costs of edges for rays of a star" $ do
+
+        let g = GAB.build $ do {
+            v0 <- GAB.vertex 0;
+            v1 <- GAB.vertex 1;
+            v2 <- GAB.vertex 2;
+            v3 <- GAB.vertex 3;
+
+            GAB.edge 2 v0 v1;
+            GAB.edge 5 v0 v2;
+            GAB.edge 42 v0 v3
+        }
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 0);
+            distances <- Dist.bford vs id;
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices (GA.zip g g') `shouldMatchList`
+            [ (0, Just 0), (1, Just 2)
+            , (2, Just 5), (3, Just 42)
+            ]
+
+    it "should be the distances of the shortest paths" $ do
+
+        let g = GAB.build $ do {
+            v0 <- GAB.vertex 0;
+            v1 <- GAB.vertex 1;
+            v2 <- GAB.vertex 2;
+            v3 <- GAB.vertex 3;
+            v4 <- GAB.vertex 4;
+
+            GAB.edge 2 v0 v1;
+            GAB.edge 2 v1 v2;
+            GAB.edge 1 v2 v4;
+            GAB.edge 3 v0 v3;
+            GAB.edge 3 v3 v4
+        }
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 0);
+            distances <- Dist.bford vs id;
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices (GA.zip g g') `shouldMatchList`
+            [ (0, Just 0), (1, Just 2)
+            , (2, Just 4), (3, Just 3)
+            , (4, Just 5)
+            ]
+
+    it "should not be distances of shortest by edges paths" $ do
+
+        let g = GAB.build $ do {
+            v0 <- GAB.vertex 0;
+            v1 <- GAB.vertex 1;
+            v2 <- GAB.vertex 2;
+            v3 <- GAB.vertex 3;
+            v4 <- GAB.vertex 4;
+            v5 <- GAB.vertex 5;
+
+            GAB.edge 20 v0 v1;
+            GAB.edge 30 v0 v2;
+            GAB.edge 2 v0 v3;
+            GAB.edge 3 v3 v4;
+            GAB.edge 4 v4 v2;
+            GAB.edge 5 v1 v5;
+            GAB.edge 1 v2 v5;
+            GAB.edge 10 v4 v5;
+            GAB.edge 1 v5 v0
+        }
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 0);
+            distances <- Dist.bford vs id;
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices (GA.zip g g') `shouldMatchList`
+            [ (0, Just 0), (1, Just 20)
+            , (2, Just 9), (3, Just 2)
+            , (4, Just 5), (5, Just 10)
+            ]
+
+    it "should break greedy approach" $ do
+
+        let g = GAB.build $ do {
+            v0 <- GAB.vertex 0;
+            v1 <- GAB.vertex 1;
+            v2 <- GAB.vertex 2;
+
+            GAB.edge (-1) v0 v1;
+            GAB.edge (-4) v1 v2;
+            GAB.edge (-3) v0 v2
+        }
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 0);
+            distances <- Dist.bford vs id;
+            traverse GAA.vgraph distances
+        }
+
+        isJust mg' `shouldBe` True
+        let g' = fromJust mg'
+
+        GA.vertices (GA.zip g g') `shouldMatchList` [(0, Just 0), (1, Just (-1)), (2, Just (-5))]
+
+    it "should be Nothing on negative cycle" $ do
+
+        let g = GAB.build $ do {
+            v0 <- GAB.vertex 0;
+            v1 <- GAB.vertex 1;
+            v2 <- GAB.vertex 2;
+            v3 <- GAB.vertex 3;
+
+            GAB.edge 1 v0 v1;
+            GAB.edge 2 v1 v2;
+            GAB.edge 2 v2 v3;
+            GAB.edge (-5) v3 v1
+        }
+
+        let mg' = GAA.execute g $ do {
+            vs <- GAA.vfind (== 0);
+            distances <- Dist.bford vs id;
+            traverse GAA.vgraph distances
+        }
+
+        isNothing mg' `shouldBe` True
+
 
 spec :: Spec
 spec = describe "Data.Graph.Abstract.Accessor.Algorithm.Distance" $ do
     dijkstraSpec
+    bfordSpec
